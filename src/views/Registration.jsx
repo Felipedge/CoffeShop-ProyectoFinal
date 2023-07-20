e20ce15a1cfbc89b48501d3c8ac8cf0df334fb8d
Felipe P <fpalmasaldivia@gmail.com>
2023-07-20 19:55:26 -0400

    se crea base para vista de registro

---
+++ src/views/Registration.jsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { useParams} from "react-router-dom";
+
+
+import React from 'react';
+
+const Registration = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Registration;
